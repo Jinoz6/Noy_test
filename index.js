f5c81923e12477c85e9531281adc7ff3b18ac941
Jinoz6 <62939379+Jinoz6@users.noreply.github.com>
2020-08-26 02:24:52 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,49 +1,91 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 function addRow() {
-    let new_name = document.getElementById("new_name").value;
-    let new_lastname = document.getElementById("new_lastname").value;
-    let new_age = document.getElementById("new_age").value;
-   
-   
-    
-    let table = document.getElementById("table_data");
-    let table_len = table.length;
-    let row = table.insertRow(table_len);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
+  let new_name = document.getElementById("new_name").value;
+  let new_lastname = document.getElementById("new_lastname").value;
+  let new_age = document.getElementById("new_age").value;
 
-    function btn() {
-        var btn_edit = document.createElement("button");
-        var edit_text = document.createTextNode("Edit");
-        btn_edit.appendChild(edit_text);
+  var table = document.getElementById("table_data");
+  var table_len = table.length;
+  var row = table.insertRow(table_len);
+  var rowCount = table.rows.length;
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
 
-        var btn_del = documenet.createElement("button");
-        var del_text = docuemnt.createTextNode("Del");
-        btn_del.appendChild(del_text);
-
-        
-
-
-        return btn_edit.outerHTML+btn_del.outerHTML;        
-        // return `<input type="button" id="edit_btn" value="Edit" onlick="editRow">`+`<input type="button" id="del_btn" value="Del onclick="delRow">`
-    }
-
-
-    cell1.innerHTML = new_name;
-    cell2.innerHTML = new_lastname;
-    cell3.innerHTML = new_age;
-    cell4.innerHTML = btn();
-
-
+  cell1.innerHTML = rowCount - 1;
+  cell2.innerHTML = new_name;
+  cell3.innerHTML = new_lastname;
+  cell4.innerHTML = new_age;
+  cell5.innerHTML = `<input type="button"  value="Edit" id="btn_edit" onclick="editRow(this)"><input type="button"  value="Del"  onclick="delRow(this)">`;
+  resetForm();
 }
 
-function editRow(){
+function editRow(td) {
+  modal.style.display = "block";
+  let selectedRow = td.parentElement.parentElement;
+  document.getElementById("new_name").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("new_lastname").value =
+    selectedRow.cells[2].innerHTML;
+  document.getElementById("new_age").value = selectedRow.cells[3].innerHTML;
+}
+function saveRow(td) {
+  var table = document.getElementById("table_data");
+  let selectedRow = td.parentElement.parentElement;
+  table.selectedRow.cells[1].innerHTML = document.getElementById(
+    "new_name"
+  ).value;
+  table.selectedRow.cells[1].innerHTML = document.getElementById(
+    "new_lastname"
+  ).value;
+  table.selectedRow.cells[1].innerHTML = document.getElementById(
+    "new_age"
+  ).value;
 
+  // selectedRow.cells[1].innerHTML = document.getElementById("new_name").value;
 
+  // selectedRow.cells[2].innerHTML = document.getElementById(
+  //   "new_lastname"
+  // ).value;
+  // selectedRow.cells[3].innerHTML = document.getElementById("new_age").value;
+
+  resetForm();
 }
 
-function delRow() {
-    
-
+function delRow(td) {
+  if (confirm("Are you sure to delete this record ?")) {
+    index = td.parentElement.parentElement.rowIndex;
+    document.getElementById("table_data").deleteRow(index);
+  }
+}
+function resetForm() {
+  document.getElementById("new_name").value = "";
+  document.getElementById("new_lastname").value = "";
+  document.getElementById("new_age").value = "";
 }
