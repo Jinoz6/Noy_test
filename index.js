3a831b9c7d7acf583e0de94a7b3eb8bec0b719ad
Jinoz6 <62939379+Jinoz6@users.noreply.github.com>
2020-09-10 15:25:26 +0700

    version with Jquery
    with jquery

--- index.js
+++ index.js
@@ -1,63 +1,64 @@
-var modal = document.getElementById("myModal");
+$(document).ready(function(){
 
-var btn = document.getElementById("myBtn");
+var modal = $("#myModal");
 
-var cancel = document.getElementById("cancel_btn");
+var btn = $("#myBtn");
+
+var cancel = $("#cancel_btn");
 
 var arrayList = [];
 
+
 var rowCount = 0;
 
-var table = document.getElementById("table_tbody");
 
-var next = document.getElementById("next");
-var pre = document.getElementById("pre");
-
-const page = new URLSearchParams(window.location.search).get('page');
-
-let arrayList1 = JSON.parse(localStorage['data']);
+var next = $("#next");
+var pre = $("#pre");
 
 
 
 
 
-btn.onclick = function () {
 
-    modal.style.display = "block";
-    document.getElementById("save_btn").style.display = "none";
 
-};
 
-cancel.onclick = function () {
 
-    modal.style.display = "none";
-    document.getElementById("add_btn").style.display = "inline";
+btn.click( function () {
+
+    modal.show();
+    $("#save_btn").hide();
+    $("#no").hide();
+
+});
+
+cancel.click ( function () {
+
+    modal.hide();
+    $("#add_btn").show()
     resetForm();
 
-};
+});
 
-window.onclick = function (event) {
 
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-
-};
 
 
 function init(){
+    
+    const page = new URLSearchParams(window.location.search).get('page');
+
+    let arrayList1 = JSON.parse(localStorage['data']);
 
     if( page >= Math.ceil(arrayList1.length/8) ){
 
-        document.getElementById("next").disabled = true;
+        $("#next").disabled = true;
 
     } 
     else if(page <= 1){
 
-        document.getElementById("pre").disabled = true;
+        $("#pre").disabled = true;
     }
 
-    table.innerHTML = " ";
+    $("tbody").empty();
 
     let urlObject = new URLSearchParams(window.location.search);
     urlObject.set("page",page);
@@ -68,80 +69,80 @@ function init(){
     setPagination(Math.ceil(arrayList1.length/8));
 }
 
-function addRow() {
+$("#add_btn").click(function () {
   
-    let new_status = document.getElementById("new_status");
+    let new_status = $("#new_status");
 
-    new_status.checked ? new_status = "Active" : new_status="Inactive";
-
-    let table_len = table.length;
-
-    let row = table.insertRow(table_len);
-
-    let tableRow = table.rows.length;
+    new_status.is(":checked") ? new_status = "Active" : new_status="Inactive";
+  
+    
+    let tableRow = $('tr').length;
+     
 
     let pages = Math.ceil(arrayList.length/8);
 
-    let rowObject = new Object();
+    let rowObject = {};
 
     rowObject.no = rowCount+1;
-    rowObject.firstname = document.getElementById("new_name").value;
-    rowObject.lastname = document.getElementById("new_lastname").value;
-    rowObject.age = document.getElementById("new_age").value;
-    rowObject.mobile = document.getElementById("new_mobile").value;
-    rowObject.address = document.getElementById("new_address").value;
+    rowObject.firstname = $("#new_name").val();
+    rowObject.lastname = $("#new_lastname").val();
+    rowObject.age = $("#new_age").val();
+    rowObject.mobile = $("#new_mobile").val();
+    rowObject.address = $("#new_address").val();
     rowObject.status = new_status;
     arrayList.push(rowObject);
     rowCount = arrayList.length;
+    
     setPagination(pages);
+    
 
     if( tableRow > 8 ){ 
         
-        document.getElementById("table_tbody").innerHTML = " ";
+        $("tbody").empty();
         
-        document.getElementById("pre").disabled = false;
-        document.getElementById("next").disabled = false;
+        $("#pre").disabled = false;
+        $("#next").disabled = false;
+        const p = new URLSearchParams(window.location.search).get('page');
+        let urlObject = new URLSearchParams(window.location.search);
+    
+        urlObject.set("page",parseInt(p)+1);
+        history.replaceState(null, null, "?"+urlObject.toString());
 
     }
     else{
 
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
-        let cell4 = row.insertCell(3);
-        let cell5 = row.insertCell(4);
-        let cell6 = row.insertCell(5);
-        let cell7 = row.insertCell(6);
-        let cell8 = row.insertCell(7);
-        
-        cell1.innerHTML = rowObject.no;
-        cell2.innerHTML = rowObject.firstname;
-        cell3.innerHTML = rowObject.lastname;
-        cell4.innerHTML = rowObject.age;
-        cell5.innerHTML = rowObject.mobile;
-        cell6.innerHTML = rowObject.address;
-        cell7.innerHTML = rowObject.status;
-        cell8.innerHTML = `<input type="image" src="edit.png" width="20" height="20"  `+
-        `value="Edit" id="btn_edit" onclick="editRow(this)">&emsp;<input type="image" `+
-        `src="bin.png" width="20" height="20"  value="Del"  onclick="delRow(this)">`;
-        localStorage.setItem("data", JSON.stringify(arrayList));
+        var html = `<tr><td>${rowObject.no}</td>`+
+        `<td>${rowObject.firstname}</td>`+
+        `<td>${rowObject.lastname}</td>`+
+        `<td>${rowObject.age}</td>`+
+        `<td>${rowObject.mobile}</td>`+
+        `<td>${rowObject.address}</td>`+
+        `<td>${rowObject.status}</td>`+
+        `<td><input type="image" src="edit.png" width="20" height="20"
+        value="Edit" id="btn_edit">`+
+        `<input type="image" src="bin.png" width="20" height="20"  value="Del" id="del_btn">`+
+        `</td></tr>`;
 
-        // modal.style.display = "none";
+        $('#table_tbody').append(html); 
         resetForm();
     }
+
+    localStorage.setItem("data", JSON.stringify(arrayList));
+    $("#new_status").prop("checked" , false);
+   
  
-}
+})
 
 function setPagination(page){
 
-    let wrapper =  document.getElementById("pagination-wrapper");
+    let wrapper =  $("#pagination-wrapper");
 
-    wrapper.innerHTML = " ";
+    wrapper.empty();
 
     for(let i= 1; i<=page;i++){
         
         let btn = pageButtons(i);
-        wrapper.appendChild(btn);
+        wrapper.append(btn);
     }
 
 }
@@ -154,9 +155,11 @@ function pageButtons(p){
     
 
     let a = document.createElement("a");
+
     a.href = `http://127.0.0.1:5500/index.html?${paramString}`;
     a.classList.add("pagination-link");
     a.innerHTML=p;
+
     return a;
  
  }
@@ -164,18 +167,18 @@ function pageButtons(p){
 
 
 
- next.addEventListener("click",function(){
+ next.click(function(){
 
     const p = new URLSearchParams(window.location.search).get('page');
     let arrayList = JSON.parse(localStorage['data']);
 
      if(p >= Math.ceil(arrayList.length/8) ){
 
-         document.getElementById("next").disabled = true;
+         $("#next").disabled = true;
 
      }else{
 
-        table.innerHTML = " ";
+        $("tbody").empty();
 
         let urlObject = new URLSearchParams(window.location.search);
 
@@ -185,23 +188,23 @@ function pageButtons(p){
 
         displayItem(8,parseInt(p)+1);
 
-        document.getElementById("pre").disabled = false;
+        $("#pre").disabled = false;
         
     }
 })
      
 
-pre.addEventListener("click",function(){
+pre.click(function(){
 
     const p = new URLSearchParams(window.location.search).get('page');
 
     if(p <= 1 ){
 
-        document.getElementById("pre").disabled = true;
+        $("#pre").disabled = true;
 
     }else{
     
-        table.innerHTML = " ";
+        $("tbody").empty();
 
         let urlObject = new URLSearchParams(window.location.search);
         urlObject.set("page",p-1);
@@ -209,7 +212,7 @@ pre.addEventListener("click",function(){
 
         displayItem(8,p-1);
 
-        document.getElementById("next").disabled = false;
+        $("#next").disabled = false;
 
     }
 })
@@ -218,146 +221,172 @@ pre.addEventListener("click",function(){
 
 function displayItem(row,p){
 
-    document.getElementById("table_tbody").innerHTML = " ";
-    console.log("show"+p);
+    $("#table_tbody").empty();
+    
 
     let page =p;
     let start = row*(page-1);
     let end = start +row;
     let arrayList = JSON.parse(localStorage['data']);
     let pagination = arrayList.slice(start,end);
+
+    
   
   
 
     for (let i= 0;i<pagination.length;i++){
 
-        let row = table.insertRow(i);
-        
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
-        let cell4 = row.insertCell(3);
-        let cell5 = row.insertCell(4);
-        let cell6 = row.insertCell(5);
-        let cell7 = row.insertCell(6);
-        let cell8 = row.insertCell(7);
+        var html = `<tr><td>${pagination[i].no}</td>`+
+        `<td>${pagination[i].firstname}</td>`+
+        `<td>${pagination[i].lastname}</td>`+
+        `<td>${pagination[i].age}</td>`+
+        `<td>${pagination[i].mobile}</td>`+
+        `<td>${pagination[i].address}</td>`+
+        `<td>${pagination[i].status}</td>`+
+        `<td><input type="image" src="edit.png" width="20" height="20"
+        value="Edit" id="btn_edit">`+
+        `<input type="image" src="bin.png" width="20" height="20"  value="Del" id="del_btn">`+
+        `</td></tr>`;
     
-        cell1.innerHTML = pagination[i].no;
-        cell2.innerHTML = pagination[i].firstname;
-        cell3.innerHTML = pagination[i].lastname;
-        cell4.innerHTML = pagination[i].age;
-        cell5.innerHTML = pagination[i].mobile;
-        cell6.innerHTML = pagination[i].address;
-        cell7.innerHTML = pagination[i].status;
-        cell8.innerHTML = `<input type="image" src="edit.png" width="20" height="20"  `+
-        `value="Edit" id="btn_edit" onclick="editRow(this)">&emsp;<input type="image" `+
-        `src="bin.png" width="20" height="20"  value="Del"  onclick="delRow(this)">`;
+        $('#table_tbody').append(html); 
+
         
     }
 
 }
 
-function editRow(td) {
+$("body").on("click","#btn_edit",function() {
 
-    modal.style.display = "block";
-    document.getElementById("add_btn").style.display = "none";
-    document.getElementById("save_btn").style.display = "inline";
+    modal.show();
+    $("#no").hide();
+    $("#add_btn").hide();
+    $("#save_btn").show();
 
-    let index = td.parentElement.parentElement.rowIndex;
-    let selectedRow = td.parentElement.parentElement;
+    let currentRow=$(this).closest("tr"); 
+         
+    let col1=currentRow.find("td:eq(0)").text(); // get current row 1st TD value
+    let col2=currentRow.find("td:eq(1)").text(); // get current row 2nd TD
+    let col3=currentRow.find("td:eq(2)").text(); // get current row 3rd TD
+    let col4=currentRow.find("td:eq(3)").text();
+    let col5=currentRow.find("td:eq(4)").text();
+    let col6=currentRow.find("td:eq(5)").text();
+    let col7=currentRow.find("td:eq(6)").text();
     
-    document.getElementById("save_btn").onclick = () => saveRow(index)
-    document.getElementById("new_name").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("new_lastname").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("new_age").value = selectedRow.cells[3].innerHTML;
-    document.getElementById("new_mobile").value = selectedRow.cells[4].innerHTML;
-    document.getElementById("new_address").value = selectedRow.cells[5].innerHTML;
 
-    const y = selectedRow.cells[6].innerHTML;
+    
+    $("#no").val(col1);
+    $("#new_name").val(col2);
+    $("#new_lastname").val(col3);
+    $("#new_age").val(col4);
+    $("#new_mobile").val(col5);
+    $("#new_address").val(col6);
+
+    const y = col7;
+    
 
     if (y === "Active"){    
 
-        document.getElementById('new_status').checked = true;
+        $("#new_status").prop("checked", true);
 
     }
     else{
 
-        document.getElementById('new_status').checked = false;
+        $("#new_status").prop("checked" , false);
     }
-
-}
-
-function saveRow(index) {
-
-    let table = document.getElementById("table_tbody").rows[index-1];
-
-    new_status.checked ? new_status = "Active" : new_status="Inactive";
-    // table.cells[0].innerHTML = (index-1);
-    table.cells[1].innerHTML = document.getElementById("new_name").value;
-    table.cells[2].innerHTML = document.getElementById("new_lastname").value;
-    table.cells[3].innerHTML = document.getElementById("new_age").value;
-    table.cells[4].innerHTML = document.getElementById("new_mobile").value;
-    table.cells[5].innerHTML = document.getElementById("new_address").value;
-    table.cells[6].innerHTML = document.getElementById("new_status").checked ? new_status = "Active" : new_status="Inactive";
     
+})
+
+$("body").on("click","#save_btn",function(){
+
+    $("#no").hide();
+
+    const p = new URLSearchParams(window.location.search).get('page');
+
+    let arrayList = JSON.parse(localStorage['data']);
+
     let rowObject = {};
+
+    let no = $("#no").val();
     
-    rowObject.no = index;
-    rowObject.firstname = document.getElementById("new_name").value;
-    rowObject.lastname = document.getElementById("new_lastname").value;
-    rowObject.age = document.getElementById("new_age").value;
-    rowObject.mobile = document.getElementById("new_mobile").value;
-    rowObject.address = document.getElementById("new_address").value;
-    rowObject.status = document.getElementById("new_status").checked ? new_status = "Active" : new_status="Inactive";
+    
+    rowObject.no = $("#no").val();
+    rowObject.firstname = $("#new_name").val();
+    rowObject.lastname = $("#new_lastname").val();
+    rowObject.age = $("#new_age").val();
+    rowObject.mobile = $("#new_mobile").val();
+    rowObject.address = $("#new_address").val();
+    rowObject.status = $("#new_status").is(":checked") ? new_status = "Active" : new_status="Inactive";
 
-    arrayList[index-1] = rowObject;
+    arrayList[parseInt(no)-1] = rowObject;
+    
+    localStorage.setItem("data", JSON.stringify(arrayList));
 
-    document.getElementById("add_btn").style.display = "inline";
-    modal.style.display = "none";
+
+    displayItem(8,parseInt(p));
+    resetForm();
+    modal.hide();
+    $("#add_btn").show();
+
     
 
     resetForm();
-}
+})
 
 function setIndex(){
 
-    let table = document.getElementById("table_tbody");
+    
 
-    rowCount = table.rows.length;
+   let rowCount = arrayList.length;
 
     for(let i=0; i<rowCount; i++)
     {
-        table.rows[i].cells[0].innerHTML = i+1;
+        arrayList[i].no = i+1;
+        localStorage.setItem("data", JSON.stringify(arrayList));
 
     }
 
 }
 
-function delRow(td) {
+$("body").on("click","#del_btn",function() {
 
-    let index = td.parentElement.parentElement.rowIndex;
     
+    const p = new URLSearchParams(window.location.search).get('page');
+    let arrayList = JSON.parse(localStorage['data']);
+    let currentRow=$(this).closest("tr");
+
+    let col1=currentRow.find("td:eq(0)").text();
+    let number = arrayList.findIndex(({no}) => no==col1);
+
+         
+ 
+   
     if (confirm("Are you sure to delete this record ?")) {
         
-        document.getElementById("table_tbody").deleteRow(index-1);
+        arrayList.splice(number,1);
     }
+    
 
-    arrayList.splice(index-1,1);
-
+    localStorage.setItem("data", JSON.stringify(arrayList));
     setIndex();
   
-}
+    displayItem(8,parseInt(p));
+    
+
+
+    
+})
 
 function resetForm() {
 
-    document.getElementById("new_name").value = "";
-    document.getElementById("new_lastname").value = "";
-    document.getElementById("new_age").value = "";
-    document.getElementById("new_mobile").value = "";
-    document.getElementById("new_address").value="";
-    document.getElementById("new_status").checked = false;
+    $("#new_name").val("");
+    $("#new_lastname").val("");
+    $("#new_age").val("");
+    $("#new_mobile").val("");
+    $("#new_address").val("");
+    $("#new_status").checked = false;
   
 }
 
+window.addEventListener("change",init());
 
-window.addEventListener("change", init());
+})
